0fd7be05ef15933911f5b1e28e033b109f44940b
Armin Hornung <hornunga@informatik.uni-freiburg.de>
2010-07-23 12:48:25 +0000

    License in cpp files, restored compatibility with boxturtle

--- octomap_server/src/octomap_server.cpp
+++ octomap_server/src/octomap_server.cpp
@@ -3,24 +3,38 @@
 * (inspired by the ROS map_saver)
 * @author A. Hornung, University of Freiburg, Copyright (C) 2009.
 * @see http://octomap.sourceforge.net/
-* License: GNU GPL v2, http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+* License: BSD
 */
 
 /*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-* for more details.
-*
-* You should have received a copy of the GNU General Public License along
-* with this program; if not, write to the Free Software Foundation, Inc.,
-* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-*/
+ * Copyright (c) 2010, A. Hornung, University of Freiburg
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of Freiburg nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 
 #include <ros/ros.h>
 #include <visualization_msgs/MarkerArray.h>
@@ -135,11 +149,15 @@ void OctomapServer::readMap(const std::string& filename){
 		//if (it->first.z() > 0.01)
 		{
 			m_occupiedCellsVis.markers[idx].points.push_back(cubeCenter);
+			/**
+			 * This only works in CTurtle:
 			if (m_useHeightMap){
 				double h = (1.0 - std::min(std::max((it->first.z()-minZ)/ (maxZ - minZ), 0.0), 1.0)) *m_colorFactor;
 				m_occupiedCellsVis.markers[idx].colors.push_back(heightMapColor(h));
 			}
 			numVoxels++;
+
+			**/
 
 		}
 	}
@@ -177,12 +195,6 @@ bool OctomapServer::serviceCallback(octomap_server::GetOctomap::Request  &req,
 }
 
 std_msgs::ColorRGBA OctomapServer::heightMapColor(double h) const {
-
-//	if (m_zMin >= m_zMax)
-//		h = 0.5;
-//	else{
-//		h = (1.0 - std::min(std::max((h-m_zMin)/ (m_zMax - m_zMin), 0.0), 1.0)) *0.8;
-//	}
 
 	std_msgs::ColorRGBA color;
 	color.a = 1.0;
