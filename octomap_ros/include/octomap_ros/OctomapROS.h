f8e3e1e9bf3caf3207d7d826792bb7f873a8c39e
Armin Hornung <hornunga@informatik.uni-freiburg.de>
2011-02-03 13:22:41 +0000

    OctomapROS added to wrap OctoMap functionality in ROS

---
+++ octomap_ros/include/octomap_ros/OctomapROS.h
@@ -0,0 +1,218 @@
+// $Id$
+
+/**
+ * OctoMap ROS integration
+ *
+ * @author A. Hornung, University of Freiburg, Copyright (C) 2011.
+ * @see http://www.ros.org/wiki/octomap_ros
+ * License: BSD
+ */
+
+/*
+ * Copyright (c) 2010, A. Hornung, University of Freiburg
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of Freiburg nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef OCTOMAP_ROS_H_
+#define OCTOMAP_ROS_H_
+
+
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/ros/conversions.h>
+
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Vector3.h>
+#include <octomap/octomap.h>
+
+namespace octomap {
+
+
+  /**
+   * @brief: ROS wrapper class for OctoMap Octrees, providing the most important
+   * functionality with ROS / PCL types.
+   * The class is templated over the Octree type. Any OcTree derived from
+   * octomap::OccupancyOcTreeBase should work. For most cases, OcTreeROS
+   * should work best for occupancy maps.
+   *
+   */
+  template <class OctreeT>
+  class OctomapROS {
+  public:
+    OctomapROS(double resolution);
+    OctomapROS(OctreeT tree);
+    virtual ~OctomapROS() {}
+
+    /**
+     * @brief Integrate a Pointcloud measurement (ROS msg, in global reference frame)
+     *
+     * @param scan PointCloud2 of measurement endpoints
+     * @param origin Origin of the scan. Any type of Point that has x,y,z member (e.g. pcl::PointXYZ or a geometry_msgs::Point) works.
+     * @param maxrange maximum range for how long individual beams are inserted (default -1: complete beam)
+     * @param pruning whether the tree is (losslessly) pruned after insertion (default: true)
+     */
+    template <class PointT>
+    void insertScan(const sensor_msgs::PointCloud2& scan, const PointT& origin,
+        double maxrange=-1., bool pruning=true);
+
+    /**
+     * @brief Integrate a Pointcloud measurement (in global reference frame)
+     *
+     * @param scan pcl PointCloud of arbitrary type. Only the members x,y,z of the Points are used.
+     * @param origin Origin of the scan. Any type of Point that has x,y,z members (e.g. pcl::PointXYZ or a geometry_msgs::Point) works.
+     * @param maxrange maximum range for how long individual beams are inserted (default -1: complete beam)
+     * @param pruning whether the tree is (losslessly) pruned after insertion (default: true)
+     */
+    template <class PointT>
+    void insertScan(const pcl::PointCloud<PointT>& scan, const PointT& origin,
+        double maxrange=-1., bool pruning=true);
+
+    /**
+     * @brief Performs raycasting in 3d.
+     *
+     * A ray is cast from origin with a given direction, the first occupied
+     * cell is returned (as center coordinate). If the starting coordinate is already
+     * occupied in the tree, this coordinate will be returned as a hit.
+     *
+     * @param origin Any type of Point that has x,y,z members (e.g. pcl::PointXYZ or a geometry_msgs::Point) works.
+     * @param direction Any type of Point that has x,y,z members (e.g. pcl::PointXYZ or a geometry_msgs::Point) works.
+     * @param end Any type of Point that has x,y,z members (e.g. pcl::PointXYZ or a geometry_msgs::Point) works.
+     * @param ignoreUnknownCells whether unknown cells are ignored. If false (default), the raycast aborts when an unkown cell is hit.
+     * @param maxRange Maximum range after which the raycast is aborted (<= 0: no limit, default)
+     * @return whether or not an occupied cell was hit
+     */
+    template <class PointT>
+    bool castRay(const PointT& origin, const PointT& direction, PointT& end,
+        bool ignoreUnknownCells = false, double maxRange = -1.0) const;
+
+    /**
+     * @brief Insert one ray between origin and end into the tree.
+     * The last node will be marked as occupied, all others on the ray as free.
+     *
+     *
+     * @param origin Any type of Point that has x,y,z members (e.g. pcl::PointXYZ or a geometry_msgs::Point) works.
+     * @param end Any type of Point that has x,y,z members (e.g. pcl::PointXYZ or a geometry_msgs::Point) works.
+     * @param maxRange
+     * @return
+     */
+    template <class PointT>
+    bool insertRay(const PointT& origin, const PointT& end, double maxRange = -1.0);
+
+
+    OctreeT octree; ///< the wrapped OctoMap octree
+  };
+
+  /**
+   * @brief The default octomap::OcTree wrapped in ROS.
+   * OcTree provides a 3D occupancy map which stores log-odds in
+   * OcTreeNodes of floats.
+   *
+   */
+  typedef OctomapROS<OcTree> OcTreeROS;
+
+
+
+  //
+  // Implementation:
+  //
+
+  template <class OctreeT>
+  OctomapROS<OctreeT>::OctomapROS(double resolution)
+      : octree(resolution)
+  {
+
+  }
+
+
+  template <class OctreeT>
+  OctomapROS<OctreeT>::OctomapROS(OctreeT tree)
+      : octree(tree)
+  {
+
+  }
+
+  template <class OctreeT>
+  template <class PCLPointT>
+  void OctomapROS<OctreeT>::insertScan(const pcl::PointCloud<PCLPointT>& scan, const PCLPointT& origin,
+      double maxrange, bool pruning){
+
+    Pointcloud pc;
+    pc.reserve(scan.points.size());
+
+    typename
+    pcl::PointCloud<PCLPointT>::const_iterator it;
+    for (it = scan.begin(); it != scan.end(); ++it){
+      pc.push_back(it->x, it->y, it->z);
+    }
+
+    octree.insertScan(pc, point3d(origin.x, origin.y, origin.z), maxrange, pruning);
+
+  }
+
+
+  template <class OctreeT>
+  template <class PointT>
+  void OctomapROS<OctreeT>::insertScan(const sensor_msgs::PointCloud2& scan, const PointT& origin,
+      double maxrange, bool pruning){
+
+
+    pcl::PointCloud<pcl::PointXYZ> pc;
+    pcl::fromROSMsg(scan, pc);
+    insertScan(pc, origin, maxrange, pruning);
+
+  }
+
+  template <class OctreeT>
+  template <class PointT>
+  bool OctomapROS<OctreeT>::insertRay(const PointT& origin, const PointT& end, double maxRange){
+
+    return octree.insertRay(point3d(origin.x, origin.y, origin.z),
+        point3d(end.x, end.y, end.z),maxRange);
+
+  }
+
+  template <class OctreeT>
+  template <class PointT>
+  bool OctomapROS<OctreeT>::castRay(const PointT& origin, const PointT& direction,
+      PointT& end, bool ignoreUnknownCells, double maxRange) const{
+
+    point3d ptEnd;
+    bool result =  octree.castRay(point3d(origin.x, origin.y, origin.z), point3d(direction.x, direction.y, direction.z),
+        ptEnd, ignoreUnknownCells, maxRange);
+
+    end.x = ptEnd.x();
+    end.y = ptEnd.y();
+    end.z = ptEnd.z();
+
+    return result;
+  }
+
+
+}
+
+#endif /* OCTOMAP_ROS_H_ */
--- octomap_ros/src/OctomapROS.cpp
+++ octomap_ros/src/OctomapROS.cpp
@@ -37,62 +37,9 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef OCTOMAP_CONVERT_MSGS_H
-#define OCTOMAP_CONVERT_MSGS_H
-
-#include <octomap/octomap.h>
-#include <octomap_ros/OctomapBinary.h>
-#include <octomap_ros/OctomapBinaryWithPose.h>
+#include <octomap_ros/OctomapROS.h>
 
 namespace octomap {
-  /**
-   * Converts an octomap map structure to a ROS octomap msg as binary data
-   *
-   * @param octomap input OcTree
-   * @param mapMsg output msg
-   */
-  static inline void octomapMapToMsg(const OcTree& octomap, octomap_ros::OctomapBinary& mapMsg){
-    // conversion via stringstream
-
-    // TODO: read directly into buffer? see
-    // http://stackoverflow.com/questions/132358/how-to-read-file-content-into-istringstream
-    std::stringstream datastream;
-    octomap.writeBinaryConst(datastream);
-    std::string datastring = datastream.str();
-    mapMsg.header.stamp = ros::Time::now();
-    mapMsg.data = std::vector<int8_t>(datastring.begin(), datastring.end());
-  }
-
-  /**
-   * Converts a ROS octomap msg (binary data) to an octomap map structure
-   *
-   * @param mapMsg
-   * @param octomap
-   */
-  static inline void octomapMsgToMap(const octomap_ros::OctomapBinary& mapMsg, octomap::OcTree& octomap){
-    std::stringstream datastream;
-    assert(mapMsg.data.size() > 0);
-    datastream.write((const char*) &mapMsg.data[0], mapMsg.data.size());
-    octomap.readBinary(datastream);
-  }
-
-  // TODO point3d_listToPCL
-
-//  PointCloud MapNode::generatePointCloud() {
-//    octomap::point3d_list node_centers;
-//    node_map->getOccupiedGlobal(node_centers);
-//    PointCloud cloud;
-//    cloud.points.reserve(node_centers.size());
-//    for (octomap::point3d_list::const_iterator it = node_centers.begin();
-//         it != node_centers.end(); ++it) {
-//      cloud.push_back(Point(it->x(), it->y(), it->z()));
-//    }
-//    return cloud;
-//  }
 
 
 }
-
-
-#endif
-
