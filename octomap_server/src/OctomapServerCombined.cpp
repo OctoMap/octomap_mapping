dda0b008635ab1abc3f763c3e7cd1fc4f92f15ca
Armin Hornung <hornunga@informatik.uni-freiburg.de>
2011-08-17 08:00:40 +0000

    updated branch with changes in trunk (rev 1798:1807):  - updated octomap package to use OctoMap 1.2.0, removed dependency on Qt / OpenGL for lib only  - fixed ground plane detection in OctomapServerCombined  - stack version updated to 0.3.3.1

--- octomap_server/src/OctomapServerCombined.cpp
+++ octomap_server/src/OctomapServerCombined.cpp
@@ -262,7 +262,7 @@ namespace octomap{
 		} else {
 			pc_nonground = pc;
 		}
-		OcTreeROS::OcTreeType::KeySet free_cells, occupied_cells;
+		KeySet free_cells, occupied_cells;
 		point3d origin = pointTfToOctomap(trans.getOrigin());
 
 		// insert ground points only as free:
@@ -303,14 +303,14 @@ namespace octomap{
 	    }
 
 	    // mark free cells only if not seen occupied in this cloud
-	    for(OcTreeROS::OcTreeType::KeySet::iterator it = free_cells.begin(), end=free_cells.end(); it!= end; ++it){
+	    for(KeySet::iterator it = free_cells.begin(), end=free_cells.end(); it!= end; ++it){
 	      if (occupied_cells.find(*it) == occupied_cells.end()){
 	    	m_octoMap.octree.updateNode(*it, false, true);
 	      }
 	    }
 
 	    // now mark all occupied cells:
-		for (OcTreeROS::OcTreeType::KeySet::iterator it = occupied_cells.begin(), end=free_cells.end(); it!= end; it++) {
+		for (KeySet::iterator it = occupied_cells.begin(), end=free_cells.end(); it!= end; it++) {
 			m_octoMap.octree.updateNode(*it, true, true);
 		}
 		m_octoMap.octree.updateInnerOccupancy();
