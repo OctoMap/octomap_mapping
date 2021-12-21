dcce675498cacfb90d979918b5cf33f7960ff521
Wolfgang Merkt <w.merkt@gmail.com>
2021-12-21 17:15:33 +0000

    [octomap_server] Fix deprecation warnings

--- octomap_server/src/octomap_server.cpp
+++ octomap_server/src/octomap_server.cpp
@@ -28,8 +28,8 @@
 
 #include <octomap_server/octomap_server.hpp>
 
-#include <tf2_eigen/tf2_eigen.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_eigen/tf2_eigen.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 #include <algorithm>
 #include <limits>
