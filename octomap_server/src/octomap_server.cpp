0c7680b852222d6031191ad01cc0ef18c0211bbb
wep21 <daisuke.nishimatsu1021@gmail.com>
2026-01-22 12:56:28 +0900

    update qos
    Signed-off-by: wep21 <daisuke.nishimatsu1021@gmail.com>

--- octomap_server/src/octomap_server.cpp
+++ octomap_server/src/octomap_server.cpp
@@ -309,7 +309,7 @@ OctomapServer::OctomapServer(const rclcpp::NodeOptions & node_options)
     std::make_shared<tf2_ros::TransformListener>(*tf2_buffer_);
 
   using std::chrono_literals::operator""s;
-  point_cloud_sub_.subscribe(this, "cloud_in", rmw_qos_profile_sensor_data);
+  point_cloud_sub_.subscribe(this, "cloud_in", rclcpp::SensorDataQoS());
   tf_point_cloud_sub_ = std::make_shared<tf2_ros::MessageFilter<PointCloud2>>(
     point_cloud_sub_, *tf2_buffer_, world_frame_id_, 5, this->get_node_logging_interface(),
     this->get_node_clock_interface(), 5s);
